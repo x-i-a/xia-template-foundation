a6d447107cf8f7a08f4eda9b3dfffbd51aac8a51
X-I-A <develop@x-i-a.com>
2024-08-31 18:01:30 +0200

    feat: supporting empty application.yaml file

--- iac/environments/base/locals.tf
+++ iac/environments/base/locals.tf
@@ -1,6 +1,6 @@
 locals {
   landscape = yamldecode(file(var.landscape_file))
-  applications = yamldecode(file(var.applications_file))
+  applications = try(yamldecode(file(var.applications_file)), {})
   modules = yamldecode(file(var.modules_file))
   environment_dict = lookup(local.landscape, "environments", {})
 }
