10268ec37067a772ac3ce512acc07a61345e04f6
X-I-A <develop@x-i-a.com>
2024-09-01 22:19:52 +0200

    feat: adding github settings

--- iac/environments/base/locals.tf
+++ iac/environments/base/locals.tf
@@ -1,8 +1,20 @@
 locals {
   landscape = yamldecode(file(var.landscape_file))
-  applications = try(yamldecode(file(var.applications_file)), {})
   modules = yamldecode(file(var.modules_file))
   environment_dict = lookup(local.landscape, "environments", {})
+}
+
+locals {
+  default_app_config = lookup(local.landscape, "applications", {})
+  applications = { for app_name, app_config in try(yamldecode(file(var.applications_file)), {}) :
+    app_name => {
+       visibility = try(app_config["visibility"], local.default_app_config["default_visibility"])
+       repository_owner = try(app_config["repository_owner"], local.default_app_config["default_repository_owner"])
+       repository_name = try(app_config["repository_name"], app_name)
+       template_owner = try(app_config["template_owner"], local.default_app_config["default_template_owner"])
+       template_name = try(app_config["template_name"], local.default_app_config["default_template_name"])
+    }
+  }
 }
 
 locals {
